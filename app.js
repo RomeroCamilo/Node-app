6acb66681a125fb1a4a9d2b9f781114b60c9872d
Camilo Romero <91925901+RomeroCamilo@users.noreply.github.com>
2023-02-19 12:38:03 -0500

    home page

--- app.js
+++ app.js
@@ -11,12 +11,18 @@ app.set('view engine','ejs');
 //linking our static assets (stylesheets, images, scripts, etc from the public folder.)
 app.use(express.static(path.join(__dirname,'public')));
 
-//function when the user requests a url page.
+//function when the user is at the login page.
 app.get('/',(request,response) =>{
     response.render("index");
     response.end();
 }
 );
+
+//function when the user is at the home page.
+app.get('/home',(request,response)=>{
+    response.render("home");
+    response.end();
+})
 
 //listening to requests on port 3000.
 app.listen(3000);
