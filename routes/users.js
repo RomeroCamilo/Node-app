441e036d272a3796aae6a1add7e3694703a3556d
Camilo Romero <91925901+RomeroCamilo@users.noreply.github.com>
2023-02-21 16:41:14 -0500

    loggedin page start

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,4 @@
-//THIS FILE WILL CONTAINER OUR URL ROUTES WHICH WE WILL MOUNT IN OUR app.js
+//THIS FILE WILL CONTAINER OUR URL ROUTES FOR /HOME WHICH WE WILL MOUNT IN OUR app.js
 //starting up our express server app.
 const express = require('express');
 const router = express.Router();
@@ -18,7 +18,6 @@ const pool  = mysql.createPool({
 router.use(bodyParser.urlencoded({ extended: true }));
 router.use(express.json());
 
-
 //routing to our home page when the user requests this url route.
 router.get('/',(request,response)=>{
     //rendering home.html
@@ -30,6 +29,11 @@ router.get('/',(request,response)=>{
 router.get('/signup',(request,response)=>{
     response.render('signup');
     response.end();
+});
+
+//function that will lead us to the sign up page.
+router.get('/loggedin',(request,response)=>{
+  response.end("Successful");
 });
 
 //exporting all our routes.
